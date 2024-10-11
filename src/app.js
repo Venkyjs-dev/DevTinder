2935b634bcbc84bccc2c28071acc45046d3ed958
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2024-10-11 23:21:58 +0530

    lession: 4 - get,post,put,patch,delete methods practice

--- src/app.js
+++ src/app.js
@@ -4,19 +4,34 @@ const express = require("express");
 // create express instance
 const app = express();
 
-// create request handler using app.use --> here, callback function, which takes req, res is request handler.
-app.use("/test", (req, res) => {
-  res.send("This is test ");
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  // handle logic to send data from db to client
+  res.send({ firstName: "Venky", lastName: "Raj" });
 });
 
-// when the /hello world route hits, then it will return the response
-app.use("/hello", (req, res) => {
-  res.send("This is hello ");
+// This will only handle POST call to /user
+app.post("/user", (req, res) => {
+  // handle logic to store data in DB
+  res.send("data saved successfully");
 });
 
-app.use("/word", (req, res) => {
-  res.send("This is test my worlds");
+// This will only handle PUT call to /user
+app.put("/user", (req, res) => {
+  // handle logic to update the user in DB
+  res.send("data updates successfully");
 });
 
+// This will only handle PATCH call to /user
+app.patch("/user", (req, res) => {
+  // handle logic to patch
+  res.send("patch updated successfully");
+});
+
+// This will only handle DELETE call to /user
+app.delete("/user", (req, res) => {
+  // handle delete logic
+  res.send("user deleted succefully");
+});
 // server listens at port 77777
 app.listen(7777);
