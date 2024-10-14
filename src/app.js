e4f0513da44e21b274abfd6aa751e27aef27ebb6
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2024-10-15 00:09:37 +0530

    lession 5: admin authentication-middleware and route handler

--- src/app.js
+++ src/app.js
@@ -4,26 +4,16 @@ const express = require("express");
 // create express instance
 const app = express();
 
-// This route path will match requests to /random.text
-app.get("/random.text", (req, res) => {
-  res.send("random.text");
+const { adminAuth } = require("./middlewares/adminAuth");
+
+app.use("/admin", adminAuth);
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("sent all data to admin!!");
 });
 
-// url: http://localhost:3000/users/34/books/8989
-app.get("/users/:userId/books/:bookId", (req, res) => {
-  console.log(req.params);
-  res.send(req.params); // { "userId": "34", "bookId": "8989" }
+app.post("/admin/deleteUser", (req, res) => {
+  res.send("user deleted successfully!!!");
 });
-
-// url: http://localhost:3000/flights/mumbai-banglore
-app.get("/flights/:from-:to", (req, res) => {
-  res.send(req.params); // { "from": "mumbai", "to": "banglore" }
-});
-
-// url: http://localhost:3000/plantae/Prunus.persica
-app.get("/plantae/:genus.:species", (req, res) => {
-  res.send(req.params); // { "genus": "Prunus", "species": "persica" }
-});
-
 // server listens at port 77777
 app.listen(8888);
