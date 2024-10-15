9e150f959c693da3293c7c23e837a6377a339e09
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2024-10-16 05:01:46 +0530

    lession 6: DB connection

--- src/app.js
+++ src/app.js
@@ -1,24 +1,15 @@
-// import express
 const express = require("express");
+const connectDB = require("./config/database.js");
 
-// create express instance
 const app = express();
 
-// error handling using try and catch
-app.get("/user", (req, res) => {
-  try {
-    throw new Error("sampe error");
-    res.send("user handler!!!");
-  } catch (e) {
-    res.status(500).send("something went wrong!!");
-  }
-});
-
-// erro handling using app.use() and err object
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("something went wrong!!");
-  }
-});
-// server listens at port 77777
-app.listen(8888);
+connectDB()
+  .then(() => {
+    console.log("DB connection made successfully...");
+    app.listen(8888, () => {
+      console.log("application started on port:8888 successfully");
+    });
+  })
+  .catch((err) => {
+    console.error("DB conncetion failed!!!!");
+  });
